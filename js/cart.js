e9b2518891eba25c80eaf8775f305a8b98a6c59b
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-01-11 21:51:40 +0500

    adding books to cart

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,38 @@
-import {booksData} from './app.js'
-console.log(booksData);
+// import {booksData} from './app.js'
+let tbody = document.querySelector('tbody')
+let totalItems = document.querySelector('#totalItems')
+let totalPrice = document.querySelector('#totalPrice')
+
+// let bookImg = localStorage.getItem('bookImg')
+// let bookName = localStorage.getItem('bookName')
+// let bookDesc = localStorage.getItem('bookDesc')
+// let bookPrice = localStorage.getItem('bookPrice')
+// let bookUrl = localStorage.getItem('bookUrl')
+// console.log(bookName);
+
+let bookInfo = localStorage.getItem('bookInfo')
+bookInfo = JSON.parse(bookInfo)
+bookInfo.forEach(el => {
+  console.log(el);
+  let num = 1
+  num++
+  let tr = document.createElement('tr')
+  tr.innerHTML = `
+      <tr>
+      <th scope="row">${num}</th>
+        <td><img src="${el.img}" alt="image" width="120px"></td>
+        <td class="fw-bolder">${el.name}</td>
+        <td>${el.desc}</td>
+        <td>${el.price}</td>
+        <td><a href="${el.link}" target="_blank">Link</a></td>
+      </tr>
+  `;
+  
+  tbody.appendChild(tr)
+  
+  let bookPrice = +el.price.slice(1);
+  totalPrice.innerHTML = bookPrice
+  
+  totalItems.innerHTML = bookInfo.length;
+});
+
